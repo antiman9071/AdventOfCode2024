c3cef59d06f18083da33bce9caaa1903d8da2d64
Anthony DiBernardo <anthony.s.dibernardo@gmail.com>
2024-12-12 02:11:00 -0500

    added functionality to use a file input class for the remainder of the challenge

--- day 1/challenge2.java
+++ day 1/challenge2.java
@@ -1,37 +1,11 @@
+package adventOfCode2024.day1;
 import java.io.*;
 import java.util.*;
-public class challenge1 {
+import adventOfCode2024.fileInput;
+public class challenge2 {
     public static void main(String[] args) {
-        //these first 13 lines are to get the file path from the user either as an argument or as a user input
-        Scanner userIn = new Scanner(System.in);
-        String filePath = ""; 
-        if(args.length > 0){
-            filePath = args[0];
-        } else {
-            System.out.println("Input a file path");
-            filePath = userIn.nextLine();
-        }
-        //this is to create the file and file scanner
-        Scanner fileIn = null;
-        File file = new File(filePath);
-        //this is the try/catch loop to open the file and catch if a file has been inputted incorrectly
-        try{
-            fileIn = new Scanner(file);
-        } catch(FileNotFoundException fnf){
-            boolean worked = false;
-            //this loop is to repeat until the user inputs the correct file
-            while(!worked){
-                System.out.println("Your file path does not exist try a different one");
-                filePath = userIn.nextLine();
-                try{
-                    file = new File(filePath);
-                    fileIn = new Scanner(file);
-                    worked = true;
-                } catch(FileNotFoundException fnf2){
-                    worked = false;
-                }
-            }
-        }
+        //this line calls to the parent class to do file input
+        Scanner fileIn = fileInput.fileIn(args);
         //these two lines set up the input for the scanner and the array so that I can separate between the two lists 
         String line = "";
         String[] lineSplit = new String[2];
---
+++ fileInput.java
@@ -0,0 +1,38 @@
+package adventOfCode2024;
+import java.util.*;
+import java.io.*;
+public class fileInput {
+    public static Scanner fileIn(String[] args){
+        //these first 13 lines are to get the file path from the user either as an argument or as a user input
+        Scanner userIn = new Scanner(System.in);
+        String filePath = "";
+        if(args.length > 0){
+            filePath = args[0];
+        } else {
+            System.out.println("Input a file path");
+            filePath = userIn.nextLine();
+        }
+        //this is to create the file and file scanner
+        Scanner fileIn = null;
+        File file = new File(filePath);
+        //this is the try/catch loop to open the file and catch if a file has been inputted incorrectly
+        try{
+            fileIn = new Scanner(file);
+        } catch(FileNotFoundException fnf){
+            boolean worked = false;
+            //this loop is to repeat until the user inputs the correct file
+            while(!worked){
+                System.out.println("Your file path does not exist try a different one");
+                filePath = userIn.nextLine();
+                try{
+                    file = new File(filePath);
+                    fileIn = new Scanner(file);
+                    worked = true;
+                } catch(FileNotFoundException fnf2){
+                    worked = false;
+                }
+            }
+        }
+        return fileIn;
+    }
+}
